2203fd637812255fb8ad2012a594b6979d0c7ae2
Sud0ck3rs <98493462+Sud0ck3rs@users.noreply.github.com>
2022-06-07 20:32:59 +0200

    Add files via upload

--- strlen.cpp
+++ strlen.cpp
@@ -1,16 +1,21 @@
 //coded by Sud0ck3rs
 #include <iostream>
-#include <string.h>
+#include <termcolor.hpp>
+
 
 using namespace std;
 
-
-int main()
+int main(int argc, char **argv)
 {
-  system("clear");
-  cout << "input strings: ";
-  string String = "";
-  getline(cin, String);
-  cout << "number char = " << strlen(String.c_str());
+  if(argc >= 2)
+  {
+    string str_Params = argv[1];
+    cout << termcolor::green << "Character number: " << termcolor::yellow << str_Params.size()
+    << termcolor::reset << endl;
+  }
+  else
+    cout << termcolor::magenta << termcolor::bold << "Usage: StrLen <character string>" 
+    << termcolor::reset << endl;
+
   return EXIT_SUCCESS;
 }
